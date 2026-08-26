2b098edf16782125fc30de23e23844b0c9d2a77f
Revive and Glow Wellness <reviveandglowwellness@gmail.com>
2026-08-26 18:13:29 -0400

    Add limited-time introductory offers

---
+++ intro-offers-config.js
@@ -0,0 +1,13 @@
+/* Revive & Glow introductory offers
+ * Set enabled to false to hide the section without removing its code.
+ * The section also hides automatically after September 25, 2026.
+ */
+window.REVIVE_INTRO_OFFERS = Object.freeze({
+  enabled: true,
+  hideAfter: '2026-09-25T23:59:59-04:00',
+  links: Object.freeze({
+    couples: 'https://clients.mindbodyonline.com/classic/ws?studioid=5755152&stype=43&prodid=100025',
+    sauna: 'https://clients.mindbodyonline.com/classic/ws?studioid=5755152&stype=43&prodid=100027',
+    redLight: 'https://clients.mindbodyonline.com/classic/ws?studioid=5755152&stype=43&prodid=100028'
+  })
+});
